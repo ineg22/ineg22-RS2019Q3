7290c2d7b9aa89145df377817ee40271b2e431b3
ineg22 <ineg22@gmail.com>
2019-11-18 02:50:39 +0300

    feat: add authorization functionality

--- codejam-image-api/src/app.js
+++ codejam-image-api/src/app.js
@@ -1,3 +1,4 @@
 import './js/helper';
 import './js/index';
+import './js/authorization';
 import './scss/base.scss';
---
+++ codejam-image-api/src/js/authorization.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+/* eslint-disable new-cap */
+const loginEl = document.querySelector('#login');
+const logoutEl = document.querySelector('#logout');
+const loginOutput = document.querySelector('#login-output');
+const userAvatar = document.querySelector('#user-avatar');
+const userAcc = document.querySelector('#user-acc');
+
+async function loginSuccess(token) {
+  const userData = await fetch('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  }).then(res => res.json());
+
+  loginEl.classList.add('hidden');
+  loginOutput.classList.remove('hidden');
+  userAvatar.style.backgroundImage = `url("${userData.avatar_url}")`;
+  userAcc.setAttribute('href', `${userData.html_url}`);
+}
+
+function loginHandler(e) {
+  e.preventDefault();
+  const authenticator = new netlify.default({});
+  authenticator.authenticate({ provider: 'github', scope: 'user' }, (err, data) => {
+    if (err) {
+      throw new Error('Authenticating error');
+    } else {
+      loginSuccess(data.token);
+    }
+  });
+}
+
+function logoutHandler() {
+  loginEl.classList.remove('hidden');
+  loginOutput.classList.add('hidden');
+}
+
+loginEl.addEventListener('click', loginHandler);
+logoutEl.addEventListener('click', logoutHandler);
