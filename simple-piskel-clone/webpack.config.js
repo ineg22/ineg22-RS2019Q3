87c888799ab0b22a55de7ca015c576523c8a429e
ineg22 <ineg22@gmail.com>
2020-01-04 19:12:35 +0300

    init: base structure, webpack

---
+++ simple-piskel-clone/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app.js',
+    landing: './src/screens/landing/app.landing.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+      // {
+      //   test: /\.(png|jpg)$/,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       outputPath: 'assets',
+      //     },
+      //   },
+      // },
+      // {
+      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       outputPath: 'fonts',
+      //     },
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.[name].css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/screens/landing/index.html',
+      chunks: ['landing'],
+      favicon: './src/screens/landing/assets/favicon-landing.png',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'sprite.html',
+      template: './src/screens/sprite/index.html',
+      chunks: ['app'],
+      favicon: './src/screens/sprite/assets/favicon-sprite.png',
+    }),
+  ],
+};
---
+++ simple-piskel-clone/worklog.md
@@ -0,0 +1,3 @@
+| Start time   | End time     | time spent | feature                                     |
+| ------------ | ------------ | ---------- | ------------------------------------------- |
+| Jan 04 17:00 | Jan 04 19:00 | 2h         | base structure of the project, webpack init |
